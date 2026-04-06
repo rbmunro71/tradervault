bc611683aa43b5a9aa3e1ff1e9097e6b63c0478d
CircleVault <rbmunro71@gmail.com>
2026-04-05 18:35:13 -0700

    Fix file picker showing media options instead of file browser on mobile

--- src/pages/Holdings.tsx
+++ src/pages/Holdings.tsx
@@ -240,7 +240,7 @@ export default function Holdings({ accountId, onHistory }: Props) {
 
                 <input
                   type="file"
-                  accept=".csv"
+                  accept=".csv,text/csv,text/plain"
                   onChange={handleFileChange}
                   style={{ marginBottom: 12, display: 'block', fontSize: 14 }}
                 />
--- src/pages/OptionsTab.tsx
+++ src/pages/OptionsTab.tsx
@@ -252,7 +252,7 @@ export default function OptionsTab({ accountId, onHistory }: Props) {
 
                 <input
                   type="file"
-                  accept=".csv"
+                  accept=".csv,text/csv,text/plain"
                   onChange={handleFileChange}
                   style={{ marginBottom: 12, display: 'block', fontSize: 14 }}
                 />
